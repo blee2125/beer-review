e5234fea02c42b864eb1be09f3223dd3806c4fd1
blee2125 <blee2125@gmail.com>
2020-07-20 10:50:16 -0400

    add actions to create new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+
+    def index
+        @beer= Beer.find_by(params[:id])
+        @comments= @beer.comments
+    end
+
+    def show
+        @comment= Comment.find_by(id: params[:id])
+    end
+
+    def new
+        @beer= Beer.find_by(params[:id])
+        
+        @comment= Comment.new
+    end
+
+    def create
+        @user= current_user.id
+        new_comment= Comment.new(comment: params[:comment][:comment], beer_id: params[:beer_id], user_id: @user)
+        new_comment.save
+        redirect_to beer_comments_path
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:comment, :user_id, :beer_id)
+    end
+
 end
