bc94becc35602b6e5b6f8f10da05422d7bd1fdfb
blee2125 <blee2125@gmail.com>
2020-07-23 16:38:14 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   get '/signin', to: 'session#new', as: 'signin'
   post '/session', to: 'session#create', as: 'session'
   delete '/session/', to: 'session#destroy'
+  get '/logout', to: 'session#destroy'
+  
 
   resources :users
-  resources :beers
+  #resources :comments
+  resources :beers do
+    resources :comments
+  end
 
 end
