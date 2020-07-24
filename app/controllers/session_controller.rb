b04708867e961d27f2f88fee226b4b5b78907a04
blee2125 <blee2125@gmail.com>
2020-07-24 15:12:28 -0400

    add flash notifications

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -11,12 +11,14 @@ class SessionController < ApplicationController
             session[:user_id]= @user.id
             redirect_to user_path(@user)
         else
+            flash[:alert]= "WRONG USERNAME/PASSWORD"
             render 'new'
         end
     end
 
     def destroy
         session.delete("user_id")
+        flash[:alert]= "YOU ARE LOGGED OUT"
         redirect_to root_path
     end
 
