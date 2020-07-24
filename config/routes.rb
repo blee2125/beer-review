f043786f5191e67d8246d22188ed431a4770fcdf
blee2125 <blee2125@gmail.com>
2020-07-24 17:48:20 -0400

    add facebook auth route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/session', to: 'session#create', as: 'session'
   delete '/session/', to: 'session#destroy'
   get '/logout', to: 'session#destroy'
+  get '/auth/facebook/callback' => 'sessions#create'
   
 
   resources :users
