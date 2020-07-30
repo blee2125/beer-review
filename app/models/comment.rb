a09391761a28f7542bbadbae5d3e44ed886268c8
blee2125 <blee2125@gmail.com>
2020-07-30 09:24:06 -0400

    added yesno method for recommends

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :beer
+
+    def yesno
+        if recommend == true
+            return "Yes"
+        else
+            return "No"
+        end
+    end
+
 end
