7c6b4cdf58b2dca9f155f1b605c1c17381b38cb3
blee2125 <blee2125@gmail.com>
2020-08-03 11:24:06 -0400

    refactor actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,14 @@ class CommentsController < ApplicationController
 
     def index
         @beer= Beer.find_by(id: params[:beer_id])
-        @comments= Comment.find_by(beer_id: params[:beer_id])
-    end
-
-    def show
-        @comment= Comment.find_by(id: params[:id])
     end
 
     def new
-        @beer= Beer.find_by(params[:id])
-        @comment= @beer.comments.build
+        beer= Beer.find_by(params[:id])
+        @comment= beer.comments.build
     end
 
     def create
-        @user= current_user.id
         @comment= Comment.create(comment_params)
         if @comment.save
             @beer= Beer.find_by(id: params[:beer_id])
