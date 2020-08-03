7eadde1e50f7ffec57d620fb89818ad0e68b40a0
blee2125 <blee2125@gmail.com>
2020-08-03 10:55:59 -0400

    validates name for uniqueness:

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,6 +5,8 @@ class Beer < ApplicationRecord
     has_many :users, through: :comments
 
     has_one_attached :beer_picture
+
+    validates :name, uniqueness: true
     
     def recommended?
         self.comments.where(recommend: true).count * 100 / self.comments.count if self.comments.count > 0
