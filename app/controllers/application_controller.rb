0bad169ee8bf34505ae16c6d0c6419bd93321ce3
blee2125 <blee2125@gmail.com>
2020-08-04 15:38:22 -0400

    added an alert to log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
 
     def verified_user
         redirect_to '/' unless user_is_authenticated
+        flash[:alert]= "PLEASE LOG IN TO VIEW" unless user_is_authenticated
     end
 
 end
