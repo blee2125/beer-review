03abce2ff47015c2d9d1bf7e967cf2a806df5287
blee2125 <blee2125@gmail.com>
2020-08-04 10:38:02 -0400

    remove ? from recommended and has many ratings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,4 @@
 class Beer < ApplicationRecord
-    has_many :ratings
-    has_many :users, through: :ratings
     has_many :comments
     has_many :users, through: :comments
 
@@ -8,7 +6,7 @@ class Beer < ApplicationRecord
 
     validates :name, uniqueness: true
     
-    def recommended?
+    def recommended
         self.comments.where(recommend: true).count * 100 / self.comments.count if self.comments.count > 0
     end
 
