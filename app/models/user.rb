91320cbbbe92108858ed37268401071307ec8e9b
blee2125 <blee2125@gmail.com>
2020-08-12 08:50:11 -0400

    remove useless code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :ratings
-    has_many :beers, through: :ratings
     has_many :comments
     has_many :beers, through: :comments
 
